6d156056e8818e52858e2dc5bf71b2716f8046fa
Nicolas Alberga <nicolasalberga@Nicolass-MacBook-Air.local>
2019-09-12 19:45:10 -0400

    practice2

---
+++ practiceNotes.js
@@ -0,0 +1,18 @@
+### The formula for the LTV - lifetime value of a customer in business
+### LTV = (ARPA * Gross margin)/ CCR
+### ARPA: average revenue per account(the average MRR across all of your active customers)
+### customer churn rate: The rate at which your customers are cancelling their subscriptions.)
+
+var ARPA = 1000
+var GM = 100
+var CCR = 20
+var LTV =(ARPA * GM)/ CCR
+console.log(LTV)
+
+### var length = 10;
+### var width = 5;
+###
+### var area = width * length;
+## console.log(area);
+
+### 
